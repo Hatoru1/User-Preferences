69f5947bcc7ec5ad5f8f689d2c6fc407a7df34d2
Alek3y1 <alek3y1@gmail.com>
2020-03-29 12:40:04 -0500

    First Commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/pages/settings_page.dart';
+
+class HomePage extends StatelessWidget {
+
+  static final String routeName = 'home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de Usuario'),
+      ),
+      drawer: _crearMenu( context ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color Secundario: '),
+          Divider(),
+          Text('Genero: '),
+          Divider(),
+          Text('Nombre Usuario: '),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.pages, color: Colors.blue,),
+            title: Text('Home'),
+            onTap: ()=>Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue,),
+            title: Text('Party Mode'),
+            onTap: (){},
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue,),
+            title: Text('People'),
+            onTap: (){},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue,),
+            title: Text('Settings'),
+            // onTap: (){
+            //   Navigator.pop(context);
+            //   Navigator.pushNamed(context, SettingsPage.routeName);
+            // },
+            onTap: () => Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+          ),
+        ],
+      ),
+    );
+  }
+}
